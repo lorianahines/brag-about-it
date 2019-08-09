46ec22d8d5826b4ffe244c771bbf3664bf825528
Loriana Hines <codeloriana@gmail.com>
2019-08-08 20:43:06 -0400

    created hashPassword and checkPassword helper functions

--- auth/auth.js
+++ auth/auth.js
@@ -3,3 +3,22 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 //set salt rounds and  token key
+const SALT_ROUNDS = 12
+const TOKEN_KEY = process.env.TOKEN_KEY
+
+//hash user's password before entering it into the database
+const hashPassword = async (password) =>{
+  const passwordDigest = await bcrypt.hash(password, SALT_ROUNDS)
+  return passwordDigest
+}
+
+//check the password when user logins 
+const checkPassword = async (password, password_digest) =>{
+  const isValidated = await bcrypt.compare(password, password_digest)
+  return isValidated
+}
+
+module.exports = {
+  hashPassword,
+  checkPassword
+}
