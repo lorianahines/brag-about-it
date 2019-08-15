7fc4e5e1ae05c3295616e994e3d71ab8a843d6f8
Loriana Hines <codeloriana@gmail.com>
2019-08-15 14:31:14 -0400

    created route to include comments

--- routes/brag.js
+++ routes/brag.js
@@ -1,18 +1,32 @@
 const { Router } = require('express')
-const { Brag } = require('../models')
+const { Brag, Comment } = require('../models')
+const { Op } = require('sequelize')
 
 const bragRouter = Router()
 
-//get all brags
+//get all brags (include comments)
 bragRouter.get('/all', async (req,res) =>{
   try{
-    const allBrags = await Brag.findAll()
+    const allBrags = await Brag.findAll({
+      raw: true,
+      include: {
+        model: Comment,
+        on:{
+          id:{
+            [Op.eq] : Comment.brag_id
+          }
+        }
+      }
+    })
     console.log(allBrags)
     res.json(allBrags)
   }catch(e){
+    console.log(e)
     res.send(e.message)
   }
 })
+
+//get
 
 
 
